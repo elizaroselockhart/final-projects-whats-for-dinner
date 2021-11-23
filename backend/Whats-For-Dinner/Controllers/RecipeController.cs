ccb7c91b996da7b2d8f25758a34525b6749deb27
Eliza Lockhart <elizaroselockhart@gmail.com>
2021-11-23 10:42:16 -0500

    added three recipes to seed data

---
+++ backend/Whats-For-Dinner/Controllers/RecipeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Whats_For_Dinner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        /*
+            HTTPPost (Create) Method goes here!
+        */
+
+
+        /*
+            HTTPGet (Read) Method goes here! We need two methods: one returns an individual recipe (by ID) and one that returns a list.
+       */
+
+        /*
+            HTTPPut (Update) method goes here!
+       */
+
+        /*
+            HTTPDelete method goes here.
+       */
+    }
+}
---
+++ backend/Whats-For-Dinner/Controllers/TagController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Whats_For_Dinner.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagController : ControllerBase
+    {
+        /*
+            HTTPPost (Create) Method goes here!
+        */
+
+
+        /*
+            HTTPGet (Read) Method goes here! We need both two methods: one that returns both an individual tag (by ID) and one that returns list.
+       */
+
+        /*
+            HTTPPut (Update) method goes here!
+       */
+
+        /*
+            HTTPDelete method goes here.
+       */
+    }
+}
