e2006f045eb6983d2475a591ca4482b6e6a2d98d
Eliza Lockhart <elizaroselockhart@gmail.com>
2021-11-23 12:04:44 -0500

    rewrote startup.cs so that we can actually access the pages we need

--- backend/Whats-For-Dinner/Startup.cs
+++ backend/Whats-For-Dinner/Startup.cs
@@ -28,10 +28,20 @@ namespace Whats_For_Dinner
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Whats_For_Dinner", Version = "v1" });
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
+
+            services.AddControllers().AddNewtonsoftJson(o =>
+            {
+                o.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                o.SerializerSettings.NullValueHandling
+                            = Newtonsoft.Json.NullValueHandling.Ignore;
             });
+            services.AddDbContext<RecipeCollectionContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,9 +50,14 @@ namespace Whats_For_Dinner
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Whats_For_Dinner v1"));
+                
             }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseCors("MyPolicy");
 
             app.UseHttpsRedirection();
 
