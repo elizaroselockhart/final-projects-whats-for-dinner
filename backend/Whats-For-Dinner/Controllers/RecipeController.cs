ffc224f93df0a931ce1f6e4736b7526c90afc5ca
Eliza Lockhart <elizaroselockhart@gmail.com>
2021-11-23 11:08:59 -0500

    implemented crud operations for recipes and tags

--- backend/Whats-For-Dinner/Controllers/RecipeController.cs
+++ backend/Whats-For-Dinner/Controllers/RecipeController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Whats_For_Dinner.Models;
 
 namespace Whats_For_Dinner.Controllers
 {
@@ -11,21 +12,59 @@ namespace Whats_For_Dinner.Controllers
     [ApiController]
     public class RecipeController : ControllerBase
     {
-        /*
-            HTTPPost (Create) Method goes here!
-        */
+        private readonly RecipeCollectionContext _db;
+        public RecipeController(RecipeCollectionContext db)
+        {
+            _db = db;
+        }
 
+        /* HTTPPost (Create) Method goes here! */
+        [HttpPost]
+        public ActionResult<Recipe> Post([FromBody] Recipe recipe)
+        {
+            _db.Recipes.Add(recipe);
+            _db.SaveChanges();
 
-        /*
-            HTTPGet (Read) Method goes here! We need two methods: one returns an individual recipe (by ID) and one that returns a list.
-       */
+            return recipe;
+        }
 
-        /*
-            HTTPPut (Update) method goes here!
-       */
+        /* HTTPGet (Read) Method goes here! We need two methods: one returns an individual recipe (by ID) and one that returns a list. */
 
-        /*
-            HTTPDelete method goes here.
-       */
+        [HttpGet]
+        public ActionResult<IEnumerable<Recipe>> Get()
+        {
+            return _db.Recipes.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Recipe> Get(int id)
+        {
+            var recipe = _db.Recipes.Find(id);
+            return recipe;
+        }
+
+        /* HTTPPut (Update) method goes here! */
+        [HttpPut("{id}")]
+        public ActionResult<Recipe> Put(int id, [FromBody] Recipe recipe)
+        {
+            if (recipe.Id == id)
+            {
+                _db.Recipes.Update(recipe);
+                _db.SaveChanges();
+            }
+
+            return recipe;
+        }
+
+        /* HTTPDelete method goes here. */
+        [HttpDelete("{id}")]
+        public ActionResult<List<Recipe>> Delete(int id)
+        {
+            var recipe = _db.Recipes.Find(id);
+            _db.Recipes.Remove(recipe);
+            _db.SaveChanges();
+
+            return _db.Recipes.ToList();
+        }
     }
 }
--- backend/Whats-For-Dinner/Controllers/TagController.cs
+++ backend/Whats-For-Dinner/Controllers/TagController.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Whats_For_Dinner.Models;
 
 namespace Whats_For_Dinner.Controllers
 {
@@ -11,21 +12,52 @@ namespace Whats_For_Dinner.Controllers
     [ApiController]
     public class TagController : ControllerBase
     {
-        /*
-            HTTPPost (Create) Method goes here!
-        */
+        private readonly RecipeCollectionContext _db;
+        public TagController(RecipeCollectionContext db)
+        {
+            _db = db;
+        }
 
+        /* HTTPPost (Create) Method goes here! */
+        [HttpPost]
+        public ActionResult<IEnumerable<Tag>> Post([FromBody] Tag tag)
+        {
+            _db.Tags.Add(tag);
+            _db.SaveChanges();
 
-        /*
-            HTTPGet (Read) Method goes here! We need both two methods: one that returns both an individual tag (by ID) and one that returns list.
-       */
+            return _db.Tags.ToList();
+        }
 
-        /*
-            HTTPPut (Update) method goes here!
-       */
+        /* HTTPGet (Read) Method goes here! We need two methods: one returns an individual recipe (by ID) and one that returns a list. */
 
-        /*
-            HTTPDelete method goes here.
-       */
+        [HttpGet]
+        public ActionResult<IEnumerable<Tag>> Get()
+        {
+            return _db.Tags.ToList();
+        }
+
+        /* HTTPPut (Update) method goes here! */
+        [HttpPut("{id}")]
+        public ActionResult<IEnumerable<Tag>> Put(int id, [FromBody] Tag tag)
+        {
+            if (tag.Id == id)
+            {
+                _db.Tags.Update(tag);
+                _db.SaveChanges();
+            }
+
+            return _db.Tags.ToList();
+        }
+
+        /* HTTPDelete method goes here. */
+        [HttpDelete("{id}")]
+        public ActionResult<List<Tag>> Delete(int id)
+        {
+            var tag = _db.Tags.Find(id);
+            _db.Tags.Remove(tag);
+            _db.SaveChanges();
+
+            return _db.Tags.ToList();
+        }
     }
 }
